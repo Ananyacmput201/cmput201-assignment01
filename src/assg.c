c14200adb3e36b60e79ce4f6e0b3dec018f9bba2
Ananya Tiwari <atiwari5@cs.ualberta.ca>
2026-09-25 13:16:39 -0600

    Adding src/assg.c and list_files.txt

--- src/assg.c
+++ src/assg.c
@@ -11,33 +11,43 @@ int main(int argc, char *argv[])
   while (fgets(line, 100, fp) != NULL){
     coount++ ;
   }  
-  printf("%d",coount);
+  printf("Processing %d schedule files...\n",coount);
 			
   fclose(fp);
 
   FILE *fopn = fopen(argv[1], "r");
   char linee[100];
+  int j = 0;
   while (fgets(linee, 100, fopn) != NULL){
-    
+    j++;
     int i = 0;
+    char lette = 'A';
     char direction;
-    for (i; linee[i] != '\0'; i++) {
-      if (linee[i] == '\n') {
-          linee[i] = '\0';}
+    for (; linee[i] != '\0'; i++) {
+      if (linee[i] == '\n'){
+      linee[i] = '\0';
+      }
+      if (linee[i] == 'X'){
+	lette = linee[i];
+      }
       if (linee[i] == 'E' || linee[i] == 'W' || linee[i] == 'N' || linee[i] == 'S'){
 	direction = linee[i];
 	      }
     }
+    
     FILE *fop = fopen(linee, "r");
     int stop_id;
     int num_stop;
     char stop[50];
-    char lin;
-    fscanf(fop, "%d%c %s %d",&stop_id,&lin, stop, &num_stop);
-    if (line != NULL){
-      printf("%d%c %d %c", stop_id,lin, num_stop, direction);}
+    
+    fscanf(fop, "%d %s %d",&stop_id, stop, &num_stop);
+    
+    if (lette =='X'){  
+      printf("schedule #%d is route %d%c it has %d stops and is in the %c direction\n",j, stop_id,lette, num_stop, direction);
+    }
     else{
-      printf("%d %d %c", stop_id, num_stop, direction);}
+      printf("schedule #%d is route %d it has %d stops and is in the %c direction\n",j, stop_id, num_stop, direction);
+    }
     fclose(fop);
   }
 
